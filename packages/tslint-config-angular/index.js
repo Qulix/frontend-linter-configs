ab99603e91023d4fad3f5a2755f9df75776e5505
Pavel Bely <poul-white@yandex.ru>
2018-12-09 21:19:18 +0300

    feat(tslint-config-angular) Added base angular tslint config
    * (tslint-config-angular): Created sym-link to environments files
    
    * feat(tslint-config-angular): Added base rules structure
    
    * feat(tslint-config-angular): Added possibility to disabling rules by environment variable

---
+++ packages/tslint-config-angular/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const {
+  RULE_SYNTAX = 'enable',
+  RULE_COMPONENT = 'enable',
+  RULE_DIRECTIVE = 'enable',
+  RULE_RX = 'enable',
+} = process.env;
+
+module.exports = {
+  exports: [
+    RULE_SYNTAX === 'enable' ? './rules/syntax.json' : null,
+    RULE_COMPONENT === 'enable' ? './rules/component.json' : null,
+    RULE_DIRECTIVE === 'enable' ? './rules/directive.json' : null,
+    RULE_RX === 'enable' ? './rules/rx.json' : null,
+  ].filter((value) => value !== null).map(require.resolve),
+}
