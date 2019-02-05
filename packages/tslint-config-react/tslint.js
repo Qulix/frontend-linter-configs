1282919838d5a4427e6d6b5fb536f0f3c58a3f51
Pavel Bely <pavelbely@yandex-team.ru>
2019-02-06 00:43:30 +0300

    feat(tslint-config-react): Added warning to rules

--- packages/tslint-config-react/tslint.js
+++ packages/tslint-config-react/tslint.js
@@ -1,26 +1,73 @@
+'use strict';
 
 module.exports = {
-  "extends": [
-    "tslint-react"
-  ],
+  extends: ['tslint-react'],
 
-  "rule": {
+  rule: {
     // @rules: https://github.com/palantir/tslint-react
-    "variable-name": false,
-    "jsx-alignment": true,
-    "jsx-ban-elements": false,
-    "jsx-ban-props": false,
-    "jsx-boolean-value": "never",
-    "jsx-curly-spacing": false,
-    "jsx-equals-spacing": "always",
-    "jsx-key": true,
-    "jsx-no-bind": true,
-    "jsx-no-lambda": true,
-    "jsx-no-multiline-js": true,
-    "jsx-no-string-ref": true,
-    "jsx-use-translation-function": "allow-punctuation",
-    "jsx-self-close": true,
-    "jsx-space-before-trailing-slash": true,
-    "jsx-wrap-multiline": true
-  }
-}
+    'variable-name': {
+      severity: 'off',
+      options: false,
+    },
+    'jsx-alignment': {
+      severity: 'warning',
+      options: true,
+    },
+    'jsx-ban-elements': {
+      severity: 'off',
+      options: false,
+    },
+    'jsx-ban-props': {
+      severity: 'off',
+      options: false,
+    },
+    'jsx-boolean-value': {
+      severity: 'warning',
+      options: 'never',
+    },
+    'jsx-curly-spacing': {
+      severity: 'warning',
+      options: true,
+    },
+    'jsx-equals-spacing': {
+      severity: 'warning',
+      options: 'always',
+    },
+    'jsx-key': {
+      severity: 'warning',
+      options: true,
+    },
+    'jsx-no-bind': {
+      severity: 'error',
+      options: true,
+    },
+    'jsx-no-lambda': {
+      severity: 'warning',
+      options: true,
+    },
+    'jsx-no-multiline-js': {
+      severity: 'error',
+      options: true,
+    },
+    'jsx-no-string-ref': {
+      severity: 'error',
+      options: true,
+    },
+    'jsx-use-translation-function': {
+      severity: 'error',
+      options: 'allow-punctuation'
+    },
+    'jsx-self-close': {
+      severity: 'error',
+      options: true,
+    },
+    'jsx-space-before-trailing-slash': {
+      severity: 'warning',
+      options: true,
+    },
+    'jsx-wrap-multiline': {
+      severity: 'warning',
+      options: true,
+    },
+  },
+};
