e0ec9eb4b0166f519354447791de0f1e09dc4e7b
Pavel Bely <pbely@qulix.com>
2019-02-17 13:27:05 +0300

    Initialized stylelint-config-styled-components

---
+++ packages/stylelint-config-styled-components/STYLEGUIDE.md
@@ -0,0 +1,3 @@
+# [Sass Style Guide](https://www.npmjs.com/package/@qulix/stylelint-config-scss)
+
+> StyleGuide under development
---
+++ packages/stylelint-config-styled-components/stylelint.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  processors: [
+    'stylelint-processor-styled-components'
+  ],
+  rules: {
+    // TODO: Need add rules
+  }
+};
+
