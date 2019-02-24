0da5f87a6cd42c71e09d2ef5a7cc1673f9018f70
Pavel Bely <pbely@qulix.com>
2019-02-24 11:49:22 +0300

    feat: Added stylelint-config-css

--- packages/stylelint-config-styled-components/stylelint.config.js
+++ packages/stylelint-config-styled-components/stylelint.config.js
@@ -1,11 +1,16 @@
 'use strict';
 
 module.exports = {
+  extends: [
+    '@qulix/stylelint-config-css'
+  ],
   processors: [
     'stylelint-processor-styled-components'
   ],
   rules: {
-    // TODO: Need add rules
-  }
+    'value-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
+    'no-empty-source': null,
+    'no-missing-end-of-source-newline': null,
+  },
 };
-
